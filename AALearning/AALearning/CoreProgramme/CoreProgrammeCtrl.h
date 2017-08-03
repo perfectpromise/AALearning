029e8c97758770d1752bf143392c697a68e175d5
perfectpromise <342528568@qq.com>
2017-08-03 09:45:09 +0800

    1、代码结构优化

---
+++ AALearning/AALearning/CoreProgramme/CoreProgrammeCtrl.h
@@ -0,0 +1,13 @@
+//
+//  CoreProgrammeCtrl.h
+//  AALearning
+//
+//  Created by LWF on 2017/8/3.
+//
+//
+
+#import "BaseViewController.h"
+
+@interface CoreProgrammeCtrl : BaseViewController
+
+@end
---
+++ AALearning/AALearning/CoreProgramme/CoreProgrammeCtrl.m
@@ -0,0 +1,38 @@
+//
+//  CoreProgrammeCtrl.m
+//  AALearning
+//
+//  Created by LWF on 2017/8/3.
+//
+//
+
+#import "CoreProgrammeCtrl.h"
+
+@interface CoreProgrammeCtrl ()
+
+@end
+
+@implementation CoreProgrammeCtrl
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.navigationItem.title = @"核心编程";
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
