c5ef0eb96978381adbf6754cb3f5672a6d436df5
perfectpromise <342528568@qq.com>
2017-11-15 09:51:02 +0800

    提交

---
+++ AALearning/AALearning/Other/Speech/SpeechViewController.h
@@ -0,0 +1,12 @@
+//
+//  SpeechViewController.h
+//  AALearning
+//
+//  Created by WS on 2017/11/15.
+//
+
+#import "BaseViewController.h"
+
+@interface SpeechViewController : BaseViewController
+
+@end
---
+++ AALearning/AALearning/Other/TextKit/TextKitViewController.h
@@ -0,0 +1,12 @@
+//
+//  TextKitViewController.h
+//  AALearning
+//
+//  Created by WS on 2017/11/15.
+//
+
+#import "BaseViewController.h"
+
+@interface TextKitViewController : BaseViewController
+
+@end
